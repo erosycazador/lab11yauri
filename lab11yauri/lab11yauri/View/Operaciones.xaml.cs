c72588a2d6991a341f06c168db94c6ca9d2b2a4e
erosycazador <erica.yauri@tecsup.edu.pe>
2023-06-13 21:15:29 -0500

    se agrega

---
+++ lab11yauri/lab11yauri/View/Operaciones.xaml.cs
@@ -0,0 +1,22 @@
+﻿using lab11yauri.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab11yauri.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operaciones : ContentPage
+    {
+        public Operaciones()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModelOperaciones();
+        }
+    }
+}
---
+++ lab11yauri/lab11yauri/ViewModels/ViewModelOperaciones.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.ComponentModel;
+
+namespace lab11yauri.ViewModels
+{
+    public class ViewModelOperaciones : ViewModelBase
+    {
+
+        int valorA;
+        public int ValorA
+        {
+            get { return valorA; }
+            set
+            {
+                if (valorA != value)
+                {
+                    valorA = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        int valorB;
+        public int ValorB
+        {
+            get { return valorB; }
+            set
+            {
+                if (valorB != value)
+                {
+                    valorB = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+        int resultadoSuma;
+        public int ResultadoSuma
+        {
+            get { return resultadoSuma; }
+            set
+            {
+                if (resultadoSuma != value)
+                {
+                    resultadoSuma = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int resultadoRestar;
+        public int ResultadoRestar
+        {
+            get { return resultadoRestar; }
+            set
+            {
+                if (resultadoRestar != value)
+                {
+                    resultadoRestar = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int resultadoMultiplicar;
+        public int ResultadoMultiplicar
+        {
+            get { return resultadoMultiplicar; }
+            set
+            {
+                if (resultadoMultiplicar != value)
+                {
+                    resultadoMultiplicar = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int resultadoDividir;
+        public int ResultadoDividir
+        {
+            get { return resultadoDividir; }
+            set
+            {
+                if (resultadoDividir != value)
+                {
+                    resultadoDividir = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand Sumar { protected set; get; }
+        public ICommand Restar { protected set; get; }
+        public ICommand Multiplicar { protected set; get; }
+        public ICommand Dividir { protected set; get; }
+
+        public ViewModelOperaciones()
+        {
+            Sumar = new Command(() =>
+            {
+                ResultadoSuma = ValorA + ValorB;
+            });
+            Restar = new Command(() =>
+            {
+                ResultadoRestar = ValorA - ValorB;
+            });
+            Multiplicar = new Command(() =>
+            {
+                ResultadoMultiplicar = ValorA * ValorB;
+            });
+            Dividir = new Command(() =>
+            {
+                ResultadoDividir = ValorA / ValorB;
+            });
+
+        }
+       
+
+
+    }
+}
